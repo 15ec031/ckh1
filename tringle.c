02c52d434aff96933a678f7551adaa976a2a9468
15ec031 <pavithravimala19@gmail.com>
2018-03-20 16:22:23 +0530

    Create tringle.c

---
+++ tringle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,area;
+printf("\nEnter the height and base of the triangel");
+scanf("%d",&a);
+scanf("\n%d",&b);
+area=0.5*a*b;
+printf("the area of the triangle is %d",area);
+return 0;
+}
